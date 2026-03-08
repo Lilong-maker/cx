7c7417b49f5373ba712c0ebcae89c80aaf21b931
pengyilong <3389926937@qq.com>
2026-03-08 19:55:34 +0800

    fix

---
+++ srv/dasic/config/goladl.go
@@ -0,0 +1,29 @@
+package config
+
+type AppConfig struct {
+	Mysql
+	Redis
+	Nacos
+}
+type Mysql struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Database string
+}
+type Redis struct {
+	Host     string
+	Port     int
+	Password string
+	Database int
+}
+type Nacos struct {
+	Addr      string
+	Port      int
+	Namespace string
+	DataID    string
+	Group     string
+	//Username  string // Nacos 用户名
+	//Password  string // Nacos 密码
+}
